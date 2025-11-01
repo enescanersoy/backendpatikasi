196ba02aad015d5bdf50a0c64c59ac95fac6bb47
Enes Can <enescanersoy8@gmail.com>
2025-11-01 11:35:44 +0300

    not ortalaması hesaplama

---
+++ notOrtalamasiHesaplama/src/main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class main {
+    static void main(String[] args)
+    {
+        int Kimya,Biyoloji, Matematik, Fizik;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Kimya Notunuzu Giriniz: ");
+        Kimya = input.nextInt();
+
+        System.out.println("Biyoloji Notunuzu Giriniz: ");
+        Biyoloji = input.nextInt();
+
+        System.out.println("Matematik Notunuzu Giriniz: ");
+        Matematik = input.nextInt();
+
+        System.out.println("Fizik Notunuzu Giriniz: ");
+        Fizik = input.nextInt();
+
+        int ortalama = (Kimya + Biyoloji + Matematik + Fizik) / 4 ;
+
+        String sonuc = (ortalama < 60) ? "Sınıfta Kaldı" : "Sınıfı Geçti";
+        System.out.println(sonuc);
+
+    }
+}
